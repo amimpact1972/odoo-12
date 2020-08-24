538f59222009a31f9c921dadb2e7c51b4ed052b5
jani435 <jani435@users.noreply.github.com>
2020-08-24 12:57:50 +0530

    fix issue
    fix server error issue

--- odoo_magento_connect/models/product_sync.py
+++ odoo_magento_connect/models/product_sync.py
@@ -250,7 +250,7 @@ class MagentoSynchronization(models.TransientModel):
                             'base_price': price,
                         }
                         getProductData.update(
-                            attribute_set_id=mageSetId, temp commit
+                            attribute_set_id=mageSetId, #temp commit for attribute id
                             visibility=4,
                             price=templateObj.list_price or 0.00,
                             sku='Template sku %s' % templateId,
